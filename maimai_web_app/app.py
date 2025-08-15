5d3f46432a4e67750f2517f427d4db451fc2c3b4
fangguan <99124722+fangguan233@users.noreply.github.com>
2025-08-15 14:39:11 +0800

    update ico

--- maimai_web_app/app.py
+++ maimai_web_app/app.py
@@ -229,6 +229,11 @@ def get_most_centered_box(boxes, img_shape):
     return most_centered
 
 # --- 主路由 ---
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'image', 'ico'),
+                               'favicon.png', mimetype='image/png')
+
 @app.route('/')
 def index():
     return render_template('index.html')
